39460eb4a8cef984174b94ddba519048cb73a359
JDX228 <90829303+JDX228@users.noreply.github.com>
2022-05-14 20:16:42 +0600

    Update anime_periodic_tasks.py

--- anime_periodic_tasks.py
+++ anime_periodic_tasks.py
@@ -7,7 +7,7 @@ import asyncio
 import httpx
 
 
-list_anime = [
+favorite_anime = [
     "Семья шпиона",
     "Мир отомэ-игр — это тяжёлый мир для мобов",
     "Госпожа Кагуя: в любви как на войне",
@@ -21,7 +21,7 @@ list_anime = [
 link = "https://naruto-base.su/novosti/drugoe_anime_ru"
 url_base = "https://naruto-base.su"
 # Количество страниц, которое будет просматривать код
-pages = 3
+pages = 5
 
 favorite_voice = 'Sibnet '
 favorite_voice = favorite_voice.lower().split()
@@ -71,46 +71,54 @@ async def main():
             url = f'{link}?page{number}'
             tasks.append(get_anime(client, url))
 
-        animes = await asyncio.gather(*tasks)
-        return animes
+        #animes = await asyncio.gather(*tasks)
+        for task in asyncio.as_completed(tasks):
+            sitemap = await task
+            soup    = BeautifulSoup(sitemap,'lxml')
+            animes  = soup.find_all('h2')
+            for anime in animes:
+                for title in favorite_anime:
+                    if title in anime.get_text():
+                        link_to_anime = f'{url_base}{anime.find("a").get("href")}'
+                        await get_sub_voice(client,link_to_anime)
 
 
-async def foo():
+# async def foo():
 
-    async with httpx.AsyncClient() as client:
-        tas = []
-        for ur in link_to_anime_list:
-            tas.append(get_sub_voice(client, ur))
+#     async with httpx.AsyncClient() as client:
+#         tas = []
+#         for ur in link_to_anime_list:
+#             tas.append(get_sub_voice(client, ur))
 
-        videoid = await asyncio.gather(*tas)
-        return videoid
+#         videoid = await asyncio.gather(*tas)
+#         return videoid
 
-link_to_anime_list = []
-# Объединяю спаршеные страницы в одну
-site = b''.join(asyncio.run(main()))
-# Перевожу в объект для парсинга
-soup = BeautifulSoup(site, "lxml")
-# Ищу все теги h2
-tag_h2_list = soup.find_all('h2')
-# Перебираю теги
-for tag_h2 in tag_h2_list:
-    # Перебираю аниме из списка аниме
-    for title in list_anime:
-        # Ищу аниме в перебираемых тегах
-        if title in tag_h2.get_text():
-            print(f'Печатаю то, что будет записываться в "title_anime":\n\t{tag_h2.get_text()}')
-            # Если аниме присутствует в название вывожу часть ссылки на страницу с аниме
-            link_to_anime = f'{url_base}{tag_h2.find("a").get("href")}'
-            print(f'Ссылка на страницу с выбором озвучки:\n\t{link_to_anime}')
-            link_to_anime_list.append(link_to_anime)
+# link_to_anime_list = []
+# # Объединяю спаршеные страницы в одну
+# site = b''.join(asyncio.run(main()))
+# # Перевожу в объект для парсинга
+# soup = BeautifulSoup(site, "lxml")
+# # Ищу все теги h2
+# tag_h2_list = soup.find_all('h2')
+# # Перебираю теги
+# for tag_h2 in tag_h2_list:
+#     # Перебираю аниме из списка аниме
+#     for title in list_anime:
+#         # Ищу аниме в перебираемых тегах
+#         if title in tag_h2.get_text():
+#             print(f'Печатаю то, что будет записываться в "title_anime":\n\t{tag_h2.get_text()}')
+#             # Если аниме присутствует в название вывожу часть ссылки на страницу с аниме
+#             link_to_anime = f'{url_base}{tag_h2.find("a").get("href")}'
+#             print(f'Ссылка на страницу с выбором озвучки:\n\t{link_to_anime}')
+#             link_to_anime_list.append(link_to_anime)
 
-print('\n\n')
+# print('\n\n')
 
 
 #ket = b''.join(asyncio.run(foo())) если доработать,мб будет работать 
-asyncio.run(foo())
+asyncio.run(main())
 for i in result:
-    print('{: <50}|{: ^25}|{: ^25}|{: >25}'.format(*result[i].values()))
+    print('{: <50}|{: ^25}|{: ^25}|{: ^25}|{: >25}'.format(*result[i].values(),i))
 
  #           print(f'{tag_h2}\n')
 #print(tag_h2)
