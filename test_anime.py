c413a7834b9db6a172ecee466f1bf4672ac372e2
basher-balance <b_438@mail.ru>
2022-05-18 22:45:48 +0600

    KEKW

--- test_anime.py
+++ test_anime.py
@@ -1,5 +1,6 @@
 import aiohttp
 import asyncio
+import httpx 
 
 from bs4 import BeautifulSoup
 
@@ -23,15 +24,16 @@ pages = 8
 
 
 async def get_response_txt(client, url):
-    async with client.get(url) as response:
-        response_txt = await response.text()
-        return response_txt
+        response = await client.get(url)
+        await asyncio.sleep(0)
+        return response.text
 
 
 async def get_link_and_episode():
 
-    async with aiohttp.ClientSession() as client:
+    async with httpx.AsyncClient() as client:
         tasks = []
+        list_link_anime = []
         for page in range(1, pages):
             page_link_drugoe_anime = f'{link_drugoe_anime}?page{page}'
             tasks.append(
@@ -42,40 +44,48 @@ async def get_link_and_episode():
                         )
                     )
         list_task_response = await asyncio.gather(*tasks)
-        global list_link_anime
-        list_link_anime = []
-        for task_response in list_task_response:
-            soup_response = BeautifulSoup(task_response, 'lxml')
-            list_soup_tag_h2 = soup_response.find_all('h2')
-            for tag_h2 in list_soup_tag_h2:
-                for title in list_anime:
-                    tag_h2_text = tag_h2.get_text()
-                    link_b = tag_h2.find("a").get("href")
-                    if title in tag_h2_text:
-                        print('название: ' + tag_h2_text)
-                        link_anime = f'{url_base}{link_b}'
-                        print(link_anime)
-                        list_link_anime.append(link_anime)
+        asdf = str(list_task_response)
+        soup_str_list_task_response = BeautifulSoup(asdf, 'lxml')
+        list_soup_tag_h2 = soup_str_list_task_response.find_all('h2')
+        print(list_soup_tag_h2)
+        for tag_h2 in list_soup_tag_h2:
+            for title in list_anime:
+                tag_h2_text = tag_h2.get_text()
+                url_anime = str(tag_h2.find("a").get("href"))
+                print(str(url_anime))
+                if title in tag_h2_text:
+                    print('название: ' + tag_h2_text)
+                    link_anime = f'{url_base}{url_anime}'
+                    print(link_anime)
+                    list_link_anime.append(link_anime)
+            break
+        return list_link_anime
 
 
 
-async def hochu():
-    async with aiohttp.ClientSession() as client:
+async def get_id_video(test):
+    async with httpx.AsyncClient() as client:
         taskq = []
-        for pager in list_link_anime:
+        for page in test:
             taskq.append(
                     asyncio.ensure_future(
                         get_response_txt(
-                            client, pager
+                            client, page
                             )
                         )
                     )
-        original_pokemonn = await asyncio.gather(*taskq)
-        for pokemon in original_pokemonn:
-            soup_str_list_respons_pages = BeautifulSoup(pokemon, 'lxml')
-            asdf = soup_str_list_respons_pages.find('a', id='ep6').get('onclick')
-            print(asdf)
+        list_task_respons = await asyncio.gather(*taskq)
+        print(list_task_respons)
+        for task_response in list_task_respons:
+            soup_str_list_respons_pages = BeautifulSoup(task_response, 'lxml')
+            try:
+                tag_a_and_id = soup_str_list_respons_pages.find('a', id='ep6').get('onclick')
+            except AttributeError:
+                pass
+            else:
+#            tag_a_and_id = soup_str_list_respons_pages.find('a', id='ep6').get('onclick').split("'")[1]
+                print(tag_a_and_id)
 
 
-asyncio.run(get_link_and_episode())
-asyncio.run(hochu())
+#asyncio.run(get_link_and_episode())
+asyncio.run(get_id_video(asyncio.run(get_link_and_episode())))
